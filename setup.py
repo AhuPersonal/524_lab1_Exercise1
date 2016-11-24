bc9c3c8be7b5cf8aa4cda22bec0158451282ebe9
Ahu Oral <ahu@alumni.ubc.ca>
2016-11-24 14:41:34 -0800

    Create package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='524_lab1_Exercise1',
+    version='0.1dev',
+	author='Ahu ORAL',
+	author_email='ahupersembe@gmail.com',
+    packages=['ahupythonpackage',],
+	url='https://github.com/AhuPersonal/524_lab1_Exercise1',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
